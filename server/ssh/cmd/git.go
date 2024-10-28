69cdf238e46d6b361a8af630128317bf40bafbaf
Zherphy <zhouyi198@h-partners.com>
2024-10-28 10:51:33 +0800

    add: ssh client
    ssh client
    request gitee.com

--- server/ssh/cmd/git.go
+++ server/ssh/cmd/git.go
@@ -1,14 +1,17 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/wish/git"
 	"github.com/metalogical/BigFiles/server/ssh/sshutils"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
 	"io"
 	"os/exec"
+	"time"
 )
 
 type Service string
@@ -22,6 +25,14 @@ type ServiceCommand struct {
 
 	// Modifier functions
 	CmdFunc func(*exec.Cmd)
+}
+
+// AuthenticateResponse is the git-lfs-authenticate JSON response object.
+type AuthenticateResponse struct {
+	Header    map[string]string `json:"header"`
+	Href      string            `json:"href"`
+	ExpiresIn time.Duration     `json:"expires_in"`
+	ExpiresAt time.Time         `json:"expires_at"`
 }
 
 const (
@@ -78,5 +89,47 @@ func (s Service) Handler(ctx context.Context, cmd ServiceCommand) error {
 	pk1 := sshutils.PublicKeyFromContext(ctx)
 	log.Printf("处理git-lfs-authenticate /n")
 	log.Printf(pk1.Type())
+	//给gitee发送ssh认证
+	cfg := &ssh.ClientConfig{
+		User:            "git",
+		HostKeyCallback: ssh.FixedHostKey(pk1),
+	}
+	client, err := ssh.Dial("tcp", "gitee.com:22", cfg)
+	if err != nil {
+		log.Printf("failed to dial: %v", err)
+	}
+	defer func(client *ssh.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Printf("failed to close ssh client: %v", err)
+		}
+	}(client)
+
+	//创建一个SSH会话
+	session, err := client.NewSession()
+	if err != nil {
+		log.Printf("failed to create session: %v", err)
+	}
+	defer func(session *ssh.Session) {
+		err := session.Close()
+		if err != nil {
+			log.Printf("failed to close session: %v", err)
+		}
+	}(session)
+
+	// 设置会话的标准输出和标准错误输出为字节缓冲区，以便获取命令执行结果
+	var stdoutBuf bytes.Buffer
+	var stderrBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+	session.Stderr = &stderrBuf
+
+	// 执行指定的指令
+	err = session.Run("git-lfs-authenticate wj00037/lfs-test.git download")
+	if err != nil {
+		log.Printf("命令执行出错: %v", err)
+		log.Printf("标准错误输出: %s", stderrBuf.String())
+	}
+	// 输出命令执行的标准输出结果
+	log.Printf("标准输出: %s", stdoutBuf.String())
 	return nil
 }
